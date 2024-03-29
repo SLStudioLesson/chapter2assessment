f91a3c5f104e6910b4799f66fe15f2ee4e1cc267
kkaintover <kenichiro.kawai@fox-hound.tech>
2024-03-29 12:12:51 +0900

    Update app/src/main/java/data/RecipeFileHandler.java
    Co-authored-by: kosuke-watanabe <kosuke.watanabe@fox-hound.jp>

--- app/src/main/java/data/RecipeFileHandler.java
+++ app/src/main/java/data/RecipeFileHandler.java
@@ -14,12 +14,18 @@ public class RecipeFileHandler {
         this.filePath = filePath;
     }
 
-    // 設問1: 一覧表示機能
+    /**
+     * 設問1: 一覧表示機能
+     * recipes.txtからレシピデータを読み込み、それをリスト形式で返します。 <br> 
+     * IOExceptionが発生したときは<i>Error reading file: 例外のメッセージ</i>とコンソールに表示します。
+     *
+     * @return レシピデータ
+     */
     public List<String> readRecipes() {
         try {
 
         } catch (IOException e) {
-
+            System.out.println("Error reading file:" + e.getMessage());
         }
         return null;
     }
